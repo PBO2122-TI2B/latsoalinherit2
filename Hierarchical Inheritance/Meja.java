b0b619d15cdbb0e046c30108f0166294ec9c4592
Dka-11 <91647072+Dka-11@users.noreply.github.com>
2021-10-19 10:43:38 +0700

    Add files via upload

---
+++ Hierarchical Inheritance/Meja.java
@@ -0,0 +1,10 @@
+public class Meja extends Kayu {
+    String bentuk = "Persegi";
+
+    public void cetakMeja(){
+        System.out.println("Model Kayu  : Meja");
+        System.out.println("Bentuk Meja : " +bentuk);
+    }
+}
+
+//Adika Ahmad Hanif Nazhir
