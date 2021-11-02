1059c78f30130349ef2e11e5d84fc7106a87924e
khrnnisak <khrnnisak07@gmail.com>
2021-11-02 12:36:58 +0700

    isi

---
+++ inheritance2/Buku2.java
@@ -0,0 +1,13 @@
+package inheritance2;
+
+public class Buku2 {
+    private int jumlahHalaman;
+
+    public void setJumlahHalaman(int newValue) {
+        jumlahHalaman = newValue;
+    }
+
+    public int getJumlahHalaman() {
+        return jumlahHalaman;
+    }
+}
---
+++ inheritance2/Novel.java
@@ -0,0 +1,45 @@
+package inheritance2;
+
+public class Novel extends Buku{
+    private String judul, penulis, penerbit;
+    private int tahunTerbit;
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public void setPenulis(String penulis) {
+        this.penulis = penulis;
+    }
+
+    public void setPenerbit(String penerbit) {
+        this.penerbit = penerbit;
+    }
+
+    public void setTahunTerbit(int tahunTerbit) {
+        this.tahunTerbit = tahunTerbit;
+    }
+    public void setJumlahHalaman(int newValue) {
+        super.setJumlahHalaman(newValue);
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public String getPenulis() {
+        return penulis;
+    }
+
+    public String getPenerbit() {
+        return penerbit;
+    }
+
+    public int getJumlahHalaman() {
+        return super.getJumlahHalaman();
+    }
+
+    public int getTahunTerbit() {
+        return tahunTerbit;
+    }
+}
