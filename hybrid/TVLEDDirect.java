d653f467718f186843257151aef8962b3c4b78ae
jasminsabila <91646765+jasminsabila@users.noreply.github.com>
2021-11-02 12:35:54 +0700

    Add files via upload

---
+++ hybrid/TVLEDDirect.java
@@ -0,0 +1,10 @@
+class TVLEDDirect extends TVLED{
+int color = 10;
+int contras = 10;
+   public void pengaturanColor(int ecolor){
+	color = ecolor);
+   }
+   public void pengaturanContras(int econtras){
+	contras = econtras);
+   }
+}
